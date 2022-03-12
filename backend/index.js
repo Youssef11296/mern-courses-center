4449658066d81688c5ed977cd80aedc8cc9deab1
Youssef Shaaban <youssef@gmail.com>
2022-03-12 08:03:41 +0200

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+// modules
+const express = require ('express');
+const mongoose = require ('mongoose');
+const cors = require ('cors');
+require ('dotenv').config ();
+
+// db setup
+mongoose.connect (process.env.MONGO_URI);
+const connection = mongoose.connection;
+connection.once ('open', () => console.log ('DB is ON!'));
+
+// server setup
+const app = express ();
+
+// server middlewares
+app.use (express.json ());
+app.use (cors ());
+
+// server configuration
+const port = process.env.PORT || 5000;
+app.listen (port, () =>
+  console.log (`The server is successfully running on port: ${port}`)
+);
